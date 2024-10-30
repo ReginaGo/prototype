77905e84bfffadf45cba4ab0e118c7a5c1b7e7bd
ReginaGo <gomezpr.2005@gmail.com>
2024-10-29 21:38:35 -0600

    2nd delivery with documentation

--- models/blogPost.js
+++ models/blogPost.js
@@ -4,6 +4,6 @@ const blogPostSchema = new mongoose.Schema({
     username: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true }
-}, { timestamps: true }); // Adds createdAt and updatedAt automatically
+}, { timestamps: true }); 
 
 module.exports = mongoose.model('BlogPost', blogPostSchema);
